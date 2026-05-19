f6baecd8ca46ab6bcc189f3f1261763dfb08a450
acysos <ignacio@acysos.com>
2026-05-19 10:37:06 +0200

    [FIX] account_statement_import_online_embat: fix warning when accountNumber is null

--- account_statement_import_online_embat/__manifest__.py
+++ account_statement_import_online_embat/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Online Bank Statements: Embat",
-    "version": "17.0.1.0.0",
+    "version": "17.0.1.0.1",
     "category": "Account",
     "website": "https://github.com/OCA/bank-statement-import",
     "author": "Acysos S.L., Odoo Community Association (OCA)",
