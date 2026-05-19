3562c1b8cb4c3418d1b8dc43cadf4094902538e6
acysos <ignacio@acysos.com>
2026-05-19 10:14:18 +0200

    [ADD] embat_account: mass update invoice in payment

--- account_embat/models/res_config_settings.py
+++ account_embat/models/res_config_settings.py
@@ -38,3 +38,17 @@ class ResConfigSettings(models.TransientModel):
             raise UserError(_("No analytic accounts found for the current company."))
         analytic_account_ids._load_embat_analytic_account()
 
+    def load_pending_invoices(self):
+        self.ensure_one()
+        if not self.embat_data_id:
+            raise UserError(_("Please configure the Embat data first."))
+        move_ids = self.env['account.move'].search([
+            ('company_id', '=', self.env.company.id),
+            ('state', '=', 'posted'),
+            ('payment_state', 'in', ['not_paid', 'partial']),
+            ('move_type', 'in', ['out_invoice', 'in_refund', 'out_receipt', 'in_invoice', 'in_receipt', 'out_refund'])
+        ])
+        if not move_ids:
+            raise UserError(_("No pending invoices found for the current company."))
+        move_ids._load_embat_move_operation()
+
