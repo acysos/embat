9f200299ccac62a368a137b80397983f4bef9499
acysos <ignacio@acysos.com>
2026-06-03 17:38:05 +0200

    [IMP] sync only company partners, sync only partial and paid invoice, fix log

--- base_embat/__manifest__.py
+++ base_embat/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Base Embat",
-    "version": "17.0.1.0.0",
+    "version": "17.0.1.0.1",
     "category": "Account",
     "website": "https://github.com/OCA/server-tools",
     "author": "Acysos S.L., Odoo Community Association (OCA)",
