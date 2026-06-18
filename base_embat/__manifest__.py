1cd62927cd4507f1a27afeef63a1572ef662c501
acysos <ignacio@acysos.com>
2026-06-18 18:38:00 +0200

    [ADD] base_embat: add res.partner.bank sync

--- base_embat/__manifest__.py
+++ base_embat/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Base Embat",
-    "version": "14.0.1.0.2",
+    "version": "14.0.1.0.3",
     "category": "Account",
     "website": "https://github.com/OCA/server-tools",
     "author": "Acysos S.L., Odoo Community Association (OCA)",
