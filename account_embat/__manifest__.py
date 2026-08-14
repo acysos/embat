9463b3824baf2ce615231cc287da21cd72d36ab1
acysos <ignacio@acysos.com>
2026-08-14 13:34:50 +0200

    [FIX] account_embat: lock embat_id

--- account_embat/__manifest__.py
+++ account_embat/__manifest__.py
@@ -16,6 +16,6 @@
         "views/res_config_settings_views.xml",
         "views/account_journal_views.xml",
         "views/account_move_views.xml",
-
+        "views/account_analytic_account_views.xml",
     ],
 }
