1fb1563d23c98f5a4f502ccdcb8faa9ecbcfd274
acysos <ignacio@acysos.com>
2026-08-14 11:05:43 +0200

    [FIX] account_embat: fix manifest version

--- account_embat/__manifest__.py
+++ account_embat/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Account: Embat",
-    "version": "19.0.1.0.3",
+    "version": "17.0.1.0.3",
     "category": "Account",
     "website": "https://github.com/OCA/account-financial-tools",
     "author": "Acysos S.L., Odoo Community Association (OCA)",
