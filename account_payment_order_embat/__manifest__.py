3df7a01811cd6e1e940f6c266e1a71731084d9f3
acysos <info@acysos.com>
2026-09-21 10:01:52 +0200

    [FIX] invoice payment

--- account_payment_order_embat/__manifest__.py
+++ account_payment_order_embat/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Account Payment Order Embat",
-    "version": "19.0.1.0.0",
+    "version": "19.0.1.0.2",
     "category": "Account",
     "author": "Acysos S.L., Odoo Community Association (OCA)",
     "license": "AGPL-3",
